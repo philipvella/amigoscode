7132f30f9da7b03eaf41b21e7c8ab19cec3e52cf
Philip Vella <philip.vella@tipico.com>
2021-08-08 18:16:51 +0200

    adding lombok and passing an object of student

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -1,4 +1,27 @@
 package com.example.demo.student;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Student {
+    private Long id;
+    @NonNull
+    private String name;
+    @NonNull
+    private Integer age;
+    @NonNull
+    private LocalDate dob;
+    @NonNull
+    private String email;
 }
