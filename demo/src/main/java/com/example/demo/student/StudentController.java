773a45ea8ceb661131b9850bd1a68aaedbf5f85d
Philip Vella <philip.vella@tipico.com>
2021-08-14 13:52:33 +0200

    Implementing the post and delete functionality

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,7 +1,11 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +24,16 @@ public class StudentController {
     @GetMapping()
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping()
+    public Student postStudent(@RequestBody Student student) {
+        return studentService.postStudent(student);
+    }
+
+    @DeleteMapping("{studentId}")
+    public void deleteStudent(@PathVariable ("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 
 }
--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -14,5 +16,20 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public Student postStudent(Student student) {
+        final Optional<Student> dbStudent = studentRepository.findStudentByEmail(student.getEmail());
+        return dbStudent
+                .map(value -> studentRepository.getById(value.getId()))
+                .orElseGet(() -> studentRepository.save(student));
+    }
+
+    public void deleteStudent(Long studentId) {
+        final boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new NoSuchElementException("Student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
     }
 }
