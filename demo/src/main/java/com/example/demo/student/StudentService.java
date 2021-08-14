64332548facab8267102f564898d0feddf4ad214
Philip Vella <philip.vella@tipico.com>
2021-08-14 14:59:50 +0200

    Implementing the put functionality updating port number adding google mvn guava plugin for better string utils

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,12 @@ package com.example.demo.student;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.google.common.base.Strings;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -28,8 +31,27 @@ public class StudentService {
     public void deleteStudent(Long studentId) {
         final boolean exists = studentRepository.existsById(studentId);
         if (!exists) {
-            throw new NoSuchElementException("Student with id " + studentId + " does not exist");
+            throw studentNotFound(studentId);
         }
         studentRepository.deleteById(studentId);
+    }
+
+    private NoSuchElementException studentNotFound(Long studentId) {
+        return new NoSuchElementException("Student with id " + studentId + " does not exist");
+    }
+
+    @Transactional
+    public void putStudent(Long studentId, String name, String email) throws IllegalStateException {
+        final Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> studentNotFound(studentId));
+        if (!Strings.isNullOrEmpty(name) && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+        if (!Strings.isNullOrEmpty(email) && !Objects.equals(student.getEmail(), email)) {
+            if (studentRepository.findStudentByEmail(email).isPresent()) {
+                throw new IllegalStateException("Email " + email + " already taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
